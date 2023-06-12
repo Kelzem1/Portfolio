1accbead804b6ff4764130ec3abbd51030221705
Kelzem1 <kelzemrod@gmail.com>
2023-06-12 07:47:26 +0200

    Components folder and first edit

---
+++ src/Components/Landingpage.jsx
@@ -0,0 +1,9 @@
+export const LandingPage = () =>{
+    return(
+        <div>
+            <h1>Rodolfo Rolon</h1>
+            <h1>Kelzem</h1>
+            <h2>Info About me...</h2>
+        </div>
+    )
+}
